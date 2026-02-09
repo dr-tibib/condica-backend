e52a1cfb3e138938b5122849d7165b4d03694ba8
Laszlo Olah <laszlo@microanalytics.ro>
2026-02-09 18:32:55 +0200

    Fix footer

--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -73,10 +73,10 @@ return [
     // ------
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'DashRocks',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://dash.rocks',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => false,
